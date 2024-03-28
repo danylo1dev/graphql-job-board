289b3fd8b93b114489910b35e080e6b8c0752a9b
danylo.hlushko.dev@gmail.com <danylo.hlushko.dev@gmail.com>
2024-03-28 20:05:41 +0200

    add: getOne resolver

---
+++ client/src/lib/graphql/queries.js
@@ -0,0 +1,41 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+const client = new GraphQLClient("http://localhost:9000/graphql");
+
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        date
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { jobs } = await client.request(query);
+  return jobs;
+}
+export async function getJob(id) {
+  const query = gql`
+    query JobById($id: ID!) {
+      jobs(id: $id) {
+        id
+        title
+        date
+        description
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const { job } = await client.request(query, { id });
+  return job;
+}
--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,11 +1,19 @@
-import { getJobs } from "../db/jobs.js";
+import { getJobs, getJob } from "../db/jobs.js";
+import { getCompany } from "../db/companies.js";
 
 export const resolvers = {
   Query: {
+    job: async (_root, { id }) => {
+      console.log(id);
+      const job = await getJob(id);
+      console.log(job);
+      return job;
+    },
     jobs: () => getJobs(),
   },
   Job: {
     date: (job) => toIsoDate(job.createdAt),
+    company: (job) => getCompany(job.companyId),
   },
 };
 
