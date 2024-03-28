38391366efddfc1a70a38aa29cb50379eba7ce74
danylo.hlushko.dev@gmail.com <danylo.hlushko.dev@gmail.com>
2024-03-28 19:10:33 +0200

    add: resolver function for job.date

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -2,8 +2,13 @@ import { getJobs } from "../db/jobs.js";
 
 export const resolvers = {
   Query: {
-    jobs: () => {
-      return getJobs();
-    },
+    jobs: () => getJobs(),
+  },
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
   },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
